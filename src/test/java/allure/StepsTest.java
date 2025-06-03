d54618aadd65c097f7517566267b2a6eb5b0819a
Marina Stepanova <123195046+marinastepanova-qa@users.noreply.github.com>
2025-06-03 15:06:11 +0300

    hw 10. Allure

---
+++ src/test/java/allure/StepsTest.java
@@ -0,0 +1,51 @@
+package allure;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
+public class StepsTest {
+    private static final String REPOSITORY = "eroshenkoam/allure-example";
+    private static final int ISSUE = 80;
+    @Test
+    public void LambdaSteps() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем главную страницу", () -> {
+            open("https://github.com");
+        });
+        step("Ищем репозиторий " + REPOSITORY, () -> {
+            $(".search-input").click();
+            $("#query-builder-test").sendKeys(REPOSITORY);
+            $("#query-builder-test").submit();
+        });
+        step("Кликаем по ссылке репозитория " + REPOSITORY, () -> {
+            $(linkText(REPOSITORY)).click();
+        });
+        step("Открываем таб Issues", () -> {
+            $("#issues-tab").click();
+        });
+        step("Проверяем наличие Issue с номером " + REPOSITORY, () -> {
+            $(withText("#" + ISSUE)).should(Condition.exist);
+        });
+
+    }
+
+    @Test
+    public void AnnotatedSteps() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        AnnotatedStepsTest steps = new AnnotatedStepsTest();
+
+        steps.openMainPage();
+        steps.searchForRepository(REPOSITORY);
+        steps.clickOnRepositoryLink(REPOSITORY);
+        steps.openIssuesTab();
+        steps.shouldSeeIssueWithNumber(ISSUE);
+    }
+}
